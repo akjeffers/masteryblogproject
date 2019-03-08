fbd3c20e77c3059bd13c0404f09866124e08ec39
akjeffers <aliciakjeffers@live.com>
2019-03-08 13:13:44 -0500

    First-Commit

---
+++ src/main/java/org/wecancodeit/masteryblogproject/MasteryblogprojectApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.masteryblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteryblogprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteryblogprojectApplication.class, args);
+	}
+
+}
