01754825f94f911aeb8420bce84f7d979b579df9
Jason Hochheiser <jsunh98@gmail.com>
2019-03-08 14:26:54 -0500

    completed all model classes

--- src/main/java/org/wecancodeit/masteryblogproject/models/Author.java
+++ src/main/java/org/wecancodeit/masteryblogproject/models/Author.java
@@ -1,5 +1,44 @@
 package org.wecancodeit.masteryblogproject.models;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Author {
+	@Id
+	@GeneratedValue
+	private Long authorId;
+	private String authorName;
+	@OneToMany(mappedBy="author")
+	private Collection<Post> posts;
+	
+	public Author () {}
+	
+	public Author(String authorName) {
+		this.authorName = authorName;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [authorId=" + authorId + ", authorName=" + authorName + "]";
+	}
+
+	public Long getAuthorId() {
+		return authorId;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public Collection<Post> getPosts() {
+		return posts;
+	}
+	
+	
 
 }
